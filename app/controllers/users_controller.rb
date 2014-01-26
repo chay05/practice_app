092e00b21186c1466225ba170b0d124e9bc44db6
Wilfred B. Opinion <pro_foxtrot02@yahoo.com>
2014-01-26 19:52:07 +0800

    Finish sign out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+      sign_in @user
       flash[:success] = "Welcome to 9GAG!"
       redirect_to @user
   	else
@@ -21,7 +22,7 @@ class UsersController < ApplicationController
   private
 
   	def user_params
-  		params.require(:user).permit(:name, :email,:password, 
+  		params.require(:user).permit(:name, :email, :password, 
   									:password_confirmation)
   	end
 end
